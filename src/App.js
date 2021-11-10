0ba548c314a9b4fe29061c57e2caf22172a4802a
Pavlo Shmygelskyi <pavlo.shmygelskyi@gmail.com>
2021-11-10 13:57:07 +0200

    It works, maybe not right but works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import createStore from './my-redux';
+import React, { useState } from 'react'
+
+
+const initialState = {
+  count: 0
+};
+const reducer = (state = initialState, action) =>{
+  console.log("Hey, I'm in reducer");
+  if (action.type === "INCREMENT") {
+    return { count: state.count + action.payload.count };
+  } else {
+    return state;
+  };
+}
+const store = createStore(reducer, initialState);
+
+const action = {
+  type: "INCREMENT",
+  payload: {
+    count: 1
+  }
+};
 
 function App() {
+  const [state, setState] = useState(initialState)
+
+  const handleClick = () => {
+    store.dispatch(action);
+    setState({ count: store.getState().count })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="App-button" onClick={handleClick}>
+        Click
+      </button>
+      You have clicked {state.count} times
     </div>
   );
 }
